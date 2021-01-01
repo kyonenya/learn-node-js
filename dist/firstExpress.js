11bb0d5e53b6f7570446f2a9b16fb6c249b532ec
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-01-01 20:17:35 +0900

    9.1 postした値の受け取り

--- dist/firstExpress.js
+++ dist/firstExpress.js
@@ -4,11 +4,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const app = express_1.default();
-const port = process.env['WEB_APP_PORT'];
+const port = 3000;
+// middleware for logging
+app
+    .use((req, res, next) => {
+    console.log(`request made to: ${req.url}`);
+    next(); // next
+});
+// encode post data
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use(express_1.default.json());
 app
     .get('/', (req, res) => {
-    res.send('Hello world.');
+    res.sendFile(path_1.default.resolve(__dirname, 'index.html'));
+})
+    .post('/submit', (req, res) => {
+    console.log(req.body);
+    res.send('Post successful!');
+})
+    .get('/items/:vagatable', (req, res) => {
+    // dynamic route
+    const veg = req.params.vagatable;
+    res.send(`This is the page for ${veg}`);
 })
     .listen(port, () => {
     console.log(`listening... port: ${port}`);
--- src/firstExpress.ts
+++ src/firstExpress.ts
@@ -1,11 +1,32 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
-const port = process.env['WEB_APP_PORT'];
+const port = 3000;
+
+// middleware for logging
+app
+  .use((req, res, next) => {
+    console.log(`request made to: ${req.url}`);
+    next(); // next
+  });
+
+// encode post data
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app
   .get('/', (req, res) => {
-    res.send('Hello world.');
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+  })
+  .post('/submit', (req, res) => {
+    console.log(req.body);
+    res.send('Post successful!');
+  })
+  .get('/items/:vagatable', (req, res) => {
+    // dynamic route
+    const veg = req.params.vagatable;
+    res.send(`This is the page for ${veg}`);
   })
   .listen(port, () => {
     console.log(`listening... port: ${port}`);
