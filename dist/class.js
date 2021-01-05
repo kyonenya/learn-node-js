c1b03381599367db6f2aeac7b73ad14f89637893
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-01-05 16:11:34 +0900

    クラスの勉強：readonly修飾子で読み取り可能かつイミュータブルにできる（冗長なgetterを書かずに済む）

--- dist/class.js
+++ dist/class.js
@@ -17,3 +17,29 @@ const post2 = new Post2({
     body: '本文本文',
 });
 console.log(post2);
+post2.title = '書き換えられたタイトル';
+console.log(post2); // title: '書き換えられたタイトル'
+class PostCapsuled {
+    constructor({ title, body }) {
+        this.title = title;
+        this.body = body;
+    }
+}
+;
+const post3 = new PostCapsuled({
+    title: 'タイトル',
+    body: '本文'
+});
+// post3.title = '書き換えられたタイトル';
+// -> "Property 'title' is protected and only accessible within class 'PostImmutable' and its subclasses."
+// console.log(post3.title);
+// -> Property 'title' is protected and only accessible within class 'PostCapsuled' and its subclasses.
+class PostImmutable {
+    constructor(title) {
+        this.title = title;
+    }
+}
+const post4 = new PostImmutable('元のタイトル');
+console.log(post4.title); // -> '元のタイトル'
+// post4.title = '書き換えられたタイトル';
+// -> Cannot assign to 'title' because it is a read-only property.
--- src/class.ts
+++ src/class.ts
@@ -25,3 +25,42 @@ const post2 = new Post2({
 });
 
 console.log(post2);
+post2.title = '書き換えられたタイトル';
+console.log(post2); // title: '書き換えられたタイトル'
+
+
+class PostCapsuled {
+  protected title;
+  protected body;
+  
+  constructor({ title, body }: {
+    title: string;
+    body: string;
+  }){
+    this.title = title;
+    this.body = body;
+  }
+};
+
+const post3 = new PostCapsuled({
+  title: 'タイトル',
+  body: '本文'
+});
+
+// post3.title = '書き換えられたタイトル';
+// -> "Property 'title' is protected and only accessible within class 'PostImmutable' and its subclasses."
+
+// console.log(post3.title);
+// -> Property 'title' is protected and only accessible within class 'PostCapsuled' and its subclasses.
+
+class PostImmutable {
+  public readonly title;
+  constructor(title: string) {
+    this.title = title;
+  }
+}
+
+const post4 = new PostImmutable('元のタイトル');
+console.log(post4.title); // -> '元のタイトル'
+// post4.title = '書き換えられたタイトル';
+// -> Cannot assign to 'title' because it is a read-only property.
