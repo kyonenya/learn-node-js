f5ee8f6483bc428aaba94124cf87b3b9c091cabc
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-01-07 08:11:46 +0900

    カリー化の勉強

--- dist/curriedFunction.js
+++ dist/curriedFunction.js
@@ -1,10 +1,13 @@
 "use strict";
+let l;
 const curriedCalcTax = (late) => (price) => price * late;
 const japaneseCalcTax = curriedCalcTax(1.08);
 const humbergerPrice = japaneseCalcTax(100);
 const execute = (params) => params.toString();
 const container = (execute) => (params) => execute(params);
-let log;
+/**
+ * repeat exec
+ */
 const repeat = (times, value) => Array(times).fill(value);
 repeat(3, 'X'); // ['X', 'X', 'X']
 /** repeatの上位互換 */
@@ -15,11 +18,36 @@ const repeatedly = (times, fn) => {
 };
 repeatedly(3, () => Math.floor(Math.random() * 10)); // [8, 5, 8]
 repeatedly(3, () => 'X'); // ['X', 'X', 'X']
-const invoker = (method, ...targets) => {
+/**
+ * invoker
+ */
+const invoke = (method, ...targets) => {
     const target = targets.shift();
     return method.apply(target, targets);
 };
-const reversed = invoker(Array.prototype.reverse, [1, 2, 3]); // [3, 2, 1]
+invoke(Array.prototype.reverse, [1, 2, 3]); // [3, 2, 1]
+const invoker = (method) => (target) => {
+    return method.apply(target, [target]);
+};
+invoker(Array.prototype.reverse)([1, 2, 3]); // [3, 2, 1]
+/**
+ * カリー化 割り算
+ */
+const leftDiv = (n) => (d) => n / d;
+const devide10By = leftDiv(10); // 10 / ?
+devide10By(2); // 5
+const rightDiv = (d) => (n) => n / d;
+const devideBy10 = rightDiv(10); // ? / 10
+devideBy10(2);
+/** right curry */
+const curry = (fn) => (second) => (first) => {
+    return fn(first, second);
+};
+const div = (n, d) => n / d;
+const _divBy10 = curry(div)(10);
+_divBy10(2); // 0.2
+// -----
 [1, 2, 3].shift(); // 1
 [[1, 2, 3]].shift(); // [1, 2, 3]
-console.log(log);
+Math.max.apply(null, [1, 2, 3]); // 3
+console.log(l);
--- src/curriedFunction.ts
+++ src/curriedFunction.ts
@@ -1,3 +1,5 @@
+let l;
+
 const curriedCalcTax = (late: number) => (price: number) => price * late;
 const japaneseCalcTax = curriedCalcTax(1.08);
 const humbergerPrice = japaneseCalcTax(100);
@@ -11,8 +13,9 @@ type _containable = (execute: dbExecutable) => (params: number) => string;
 const execute: dbExecutable = (params) => params.toString();
 const container: containable = (execute: dbExecutable) => (params: number) => execute(params);
 
-let log;
-
+/**
+ * repeat exec
+ */
 const repeat = <T>(times: number, value: T): T[] => Array(times).fill(value);
 repeat(3, 'X'); // ['X', 'X', 'X']
 
@@ -25,15 +28,46 @@ const repeatedly = (times: number, fn: Function) => {
 repeatedly(3, () => Math.floor(Math.random() * 10)); // [8, 5, 8]
 repeatedly(3, () => 'X'); // ['X', 'X', 'X']
 
-const invoker = (method: Function, ...targets: any) => {
+/** 
+ * invoker
+ */
+const invoke = (method: Function, ...targets: any) => {
   const target = targets.shift();
   return method.apply(target, targets);
 };
 
-const reversed = invoker(Array.prototype.reverse, [1, 2, 3]); // [3, 2, 1]
+invoke(Array.prototype.reverse, [1, 2, 3]); // [3, 2, 1]
 
+const invoker = (method: Function) => (target: unknown[]) => {
+  return method.apply(target, [target]);
+};
+
+invoker(Array.prototype.reverse)([1, 2, 3]); // [3, 2, 1]
+
+/** 
+ * カリー化 割り算
+ */
+const leftDiv = (n: number) => (d: number) => n / d;
+const devide10By = leftDiv(10); // 10 / ?
+devide10By(2); // 5
+
+const rightDiv = (d: number) => (n: number) => n / d;
+const devideBy10 = rightDiv(10); // ? / 10
+devideBy10(2);
+
+/** right curry */
+const curry = (fn: Function) => (second: unknown) => (first: unknown) => {
+  return fn(first, second);
+};
+
+const div = (n: number, d: number) => n / d;
+const _divBy10 = curry(div)(10);
+_divBy10(2); // 0.2
+
+// -----
 [1, 2, 3].shift(); // 1
 [[1, 2, 3]].shift(); // [1, 2, 3]
 
+Math.max.apply(null, [1, 2, 3]); // 3
 
-console.log(log)
+console.log(l);
