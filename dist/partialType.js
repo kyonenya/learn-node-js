8bfa2e6ff68dc0f0a9788e039fbedebdb7b808c9
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-01-08 21:45:42 +0900

    部分型について追記

--- dist/partialType.js
+++ dist/partialType.js
@@ -9,6 +9,9 @@ hello(args); // -> OK
 const f = (args) => ({ s: args.s });
 // const funcA: F1 = f; // -> Error TS2322 引数は付け足してはいけない
 const funcB = f; // -> OK 引数は欠けてもよい
+const params = { n: 123, s: 'abc' };
+// funcB(params); // -> Error S2345 実際に呼び出すときに引数が欠けてはならない
+funcB({ n: 123, s: 'abc', b: true }); // OK 使わない引数まで含めて型定義した分すべて揃えて呼んでいる
 const g = (args) => ({ s: args.s, n: args.n });
 const func1 = g; // -> OK 返り値は余計なものを付け足してもいい
 // const func2: G2 = g; // -> Error 返り値は欠けてはいけない
@@ -21,6 +24,20 @@ const func1 = g; // -> OK 返り値は余計なものを付け足してもいい
     // useCase(param); =>
     const repository = ({ execute, param }) => {
         const sql = `INSERT INTO posts ... VALUES ${param})`;
+        return execute(sql);
+    };
+    const execute = (sql) => null;
+}
+{
+    const controller = (execute, repository) => {
+        const useCase = ({}) => repository({ execute });
+        //  const args = {}
+        //  const re = useCase(args);
+        return useCase;
+    };
+    // useCase(param); =>
+    const repository = ({ execute }) => {
+        const sql = `INSERT INTO posts ... VALUES`;
         return execute(sql);
     };
     const execute = (sql) => null;
--- src/partialType.ts
+++ src/partialType.ts
@@ -16,6 +16,9 @@ const f = (args: {n: number, s: string }) => ({ s: args.s });
 type F2 = (args: { n: number, s: string, b: boolean }) => { s: string };
 // const funcA: F1 = f; // -> Error TS2322 引数は付け足してはいけない
 const funcB: F2 = f; // -> OK 引数は欠けてもよい
+const params = { n: 123, s: 'abc'};
+// funcB(params); // -> Error S2345 実際に呼び出すときに引数が欠けてはならない
+funcB({ n: 123, s: 'abc', b: true }); // OK 使わない引数まで含めて型定義した分すべて揃えて呼んでいる
 
 /** return values */
 type nsb = { n: number, s: string, b: boolean };
@@ -46,6 +49,30 @@ const controller: controllable = (execute: executable, repository: repositorable
 // useCase(param); =>
 const repository: repositorable = ({ execute, param }): void => {
   const sql = `INSERT INTO posts ... VALUES ${param})`;
+  return execute(sql);
+};
+const execute = (sql: string) => null;
+
+}
+
+
+{
+
+type executable = (sql: string) => void;
+type repositorable = ({ execute }: {
+  execute: executable; // optional
+}) => void;
+type controllable = (execute: executable, repository: repositorable) => repositorable;
+
+const controller: controllable = (execute: executable, repository: repositorable) => {
+  const useCase: repositorable = ({}) => repository({ execute });
+//  const args = {}
+//  const re = useCase(args);
+  return useCase;
+};
+// useCase(param); =>
+const repository: repositorable = ({ execute }): void => {
+  const sql = `INSERT INTO posts ... VALUES`;
   return execute(sql);
 };
 const execute = (sql: string) => null;
