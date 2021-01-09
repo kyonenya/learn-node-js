33eac530feb8f6a81f9f63452c5fc7ed681ae380
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-01-09 13:10:59 +0900

    invokerパターン完成。setterなど不要で、invokerの引数に生データを渡せばいい

--- dist/closure.js
+++ dist/closure.js
@@ -1,14 +1,26 @@
 "use strict";
 {
-    const repository = (execute) => {
-        // const param = { name: 'posts' };
-        let param = { name: '' };
-        const setParam = (arg) => param = arg;
-        const sql = `SELECT * FROM ${param.name}`;
-        return [() => execute(sql), setParam];
+    ;
+    const repository = (executor) => {
+        return (post) => {
+            const sql = `INSERT INTO posts (body) VALUES (${post.body})`;
+            executor(sql);
+        };
     };
-    const execute = (sql) => console.log(sql);
-    const [invoke, setParam] = repository(execute);
-    setParam({ name: 'manuscripts' });
-    invoke();
+    const executor = (sql) => console.log(sql);
+    const invoker = repository(executor);
+    invoker({ body: '本文' });
+    const counter = () => {
+        let count = 0;
+        const countUp = () => {
+            count++;
+            console.log(count);
+        };
+        console.log(`count: ${count}`);
+        return [countUp];
+    };
+    const [countUp] = counter();
+    countUp();
+    countUp();
+    counter(); // -> count: 0 リセットされてしまう
 }
--- src/closure.ts
+++ src/closure.ts
@@ -1,14 +1,32 @@
 {
-const repository = (execute: Function): [Function, Function] => {
-  // const param = { name: 'posts' };
-  let param = { name: '' };
-  const setParam = (arg: any): void => param = arg;
-  const sql = `SELECT * FROM ${param.name}`;
-  return [() => execute(sql), setParam];
+interface invokable {
+  (params: any): void
+};
+const repository = (executor: Function): invokable => {
+  return (post: {
+    body: string
+  }) => {
+    const sql = `INSERT INTO posts (body) VALUES (${post.body})`;
+    executor(sql);
+  }
 };
 
-const execute = (sql: string) => console.log(sql);
-const [invoke, setParam] = repository(execute);
-setParam({ name: 'manuscripts' });
-invoke();
+const executor = (sql: string) => console.log(sql);
+const invoker = repository(executor);
+invoker({ body: '本文' });
+
+const counter = () => {
+  let count = 0;
+  const countUp = () => {
+    count++;
+    console.log(count);
+  };
+  console.log(`count: ${count}`);
+  return [countUp];
+};
+
+const [countUp] = counter();
+countUp();
+countUp();
+counter(); // -> count: 0 リセットされてしまう
 }
